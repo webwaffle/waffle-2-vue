7760669a7a34e88ace41dcc007a19ccdabdb6de4
webwaffle <sam525watchman@gmail.com>
2018-07-27 13:47:39 -0400

    Started login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="login">
+        <input type="text" id="username" v-model="username">
+        <input type="password" id="password" v-model="password">
+        <button id="login" @click="logIn">Login</button>
+        <p v-if="error">{{ error }}</p>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    data() {
+        return {
+            username: '',
+            password: '',
+            error: ''
+        }
+    },
+    methods: {
+        logIn() {
+            console.log('Logging in...')
+            axios.post('http://' + location.hostname + ':3000/login', {
+                username: this.username,
+                password: this.password
+            })
+            .then((response) => {
+                //console.log(response);
+                if(response.success) {
+                    console.log('logged in');
+                } else {
+                    this.error = response.error
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
+    }
 }
 </script>
