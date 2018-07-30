b99a602eb810b2e0fb5fdd8fdb26eb6212a290ff
webwaffle <sam525watchman@gmail.com>
2018-07-30 18:28:53 -0400

    Started Login again

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login-form">
+    <input type="text" v-model="username" placeholder="Username"><br>
+    <input type="password" v-model="password" value="Password"><br>
+    <button @click="loginUser">Login</button>
+    <p class="success-text"></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {},
+  }
+</script>
