f0acdad3ed1b7992ced41e36507a684fc486fd54
webwaffle <sam525watchman@gmail.com>
2018-08-23 10:58:29 -0400

    Started chats

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,20 +4,43 @@
     <router-link to="/create-post">Create a post</router-link>
     <p v-if="!checkLoggedIn" class="error animated bounceInLeft">You are not logged in.</p>
     <a @click="logout" v-else>Logout</a>
+    <div class="chat" v-if="chats.length" v-for="chat in chats" :key="chat.id">
+      <h2>{{ chat.name }}</h2>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  name: 'Sidebar',
   computed: {
      checkLoggedIn() {
        return !!this.$store.state.user.username
      }
   },
+  data() {
+    return {
+      chats: []
+    }
+  },
   methods: {
     logout() {
       localStorage.removeItem('vuex');
       this.$store.commit('logout');
+    }
+  },
+  created() {
+    if(this.$store.state.user.username) {
+      axios.get('http://' + location.hostname + ':3000/get-chats?key=' + this.$store.state.user.apiKey)
+      .then((res) => {
+        if(res.data.success) {
+          this.chats = res.data.chats
+        } else {
+          console.log(res)
+        }
+      })
+      .catch(console.log)
     }
   }
 }
